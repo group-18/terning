be6fc76142b20c038e2824263876eee1451a59f6
Magnus Hansen <magnus.h1993@gmail.com>
2017-10-02 16:30:21 +0200

    Add initial test

---
+++ test/terning/DiceTest.java
@@ -0,0 +1,26 @@
+package terning;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void roll() throws Exception {
+    }
+
+    @Test
+    public void rollMultiple() throws Exception {
+    }
+
+}
