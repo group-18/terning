df9676d9a66189283d42aff9a55791709c8c5104
Magnus Hansen <magnus.h1993@gmail.com>
2017-10-02 18:07:11 +0200

    Fixing random roll on Dice

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -1,12 +1,19 @@
 package terning;
 
+import java.util.Random;
+
 public class Dice {
+	private Random rand;
+
+
+	public Dice() {
+		this.rand = new Random();
+	}
+
+
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		return this.rand.nextInt(6) + 1;
 	}
 	
 	// roll the die n times and print the values
